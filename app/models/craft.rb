c034a54df0a955e5b780806a9ff452116c7157b4
Max Wofford <max.speed.wofford@gmail.com>
2014-12-30 14:27:07 -0800

    craft model validates craft_file and name

--- app/models/craft.rb
+++ app/models/craft.rb
@@ -1,4 +1,6 @@
 class Craft < ActiveRecord::Base
   acts_as_taggable # Alias for acts_as_taggable_on :tags
   acts_as_taggable_on :tags
+  validates :name, presence: {message: 'Name cannot be blank, Craft not saved'}
+  validates :craft_file, presence: {message: 'Craft file cannot be blank, Craft not saved'}
 end
